d1928367323025a7c27575d1f8a49d09784bed17
Alex Kinnear <alexander.kinnear@aggiemail.usu.edu>
2020-03-14 21:56:46 -0600

    Leftist Heap implimented

--- src/LeftistHeap.java
+++ src/LeftistHeap.java
@@ -1,71 +1,107 @@
-
-class Node<E extends Comparable<E>> {
-    E element;
-    int pathToNull;
-    Node<E> left;
-    Node<E> right;
-
-    public Node(E element) {
-        this(element, null, null);
-    }
-
-    public Node(E element, Node<E> left, Node<E> right) {
-        this.element = element;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 public class LeftistHeap<E extends Comparable<E>> {
-    private Node root;
 
-    LeftistHeap() {
+    private LeftistNode<E> root;
+
+    public LeftistHeap() {
         root = null;
     }
 
+    public void merge(LeftistHeap<E> rightSubTree) {
+        if(this == rightSubTree)
+            return;
+
+        root = merge(root, rightSubTree.root);
+        rightSubTree.root = null;
+    }
+
+    private LeftistNode<E> merge(LeftistNode<E> tree1, LeftistNode<E> tree2) {
+        if(tree1 == null)
+            return tree2;
+        if(tree2 == null)
+            return tree1;
+        if(tree1.element.compareTo(tree2.element) < 0)
+            return mergeHelper(tree1, tree2);
+        else
+            return mergeHelper(tree2, tree1);
+    }
+
+
+    private LeftistNode<E> mergeHelper(LeftistNode<E> tree1, LeftistNode<E> tree2) {
+        if(tree1.left == null)
+            tree1.left = tree2;
+        else {
+            tree1.right = merge(tree1.right, tree2);
+            if(tree1.left.pathToNull < tree1.right.pathToNull)
+                swapChildren(tree1);
+            tree1.pathToNull = tree1.right.pathToNull + 1;
+        }
+        return tree1;
+    }
+
+
+    private static <E> void swapChildren(LeftistNode<E> node) {
+        LeftistNode<E> temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+
+
     public void insert(E node) {
-        Node newNode = new Node(node);
+        LeftistNode<E> newNode = new LeftistNode<E>(node);
         root = merge(newNode, root);
     }
 
-    public E deleteMin() {
-        if (root == null) {
-            return null;
-        }
 
-        E minElement = (E) root.element;
-        root = merge(root.left, root.right);
+    public E deleteMin( ) {
+        if(root == null)
+            return null;
+
+        E minElement = root.element;
+        root = merge( root.left, root.right);
+
         return minElement;
     }
 
-    private Node merge(Node n1, Node n2) {
-        if (n1 == null) {
-            return n2;
-        }
-        if (n2 == null) {
-            return n1;
-        }
-        if (n1.element.compareTo(n2.element) < 0) {
-            Node temp = n1;
-            n1 = n2;
-            n2 = temp;
+    public void printTree() {
+        printTree(root, "");
+    }
+    private void printTree(LeftistNode<E> node, String indent) {
+        if (node == null) return;
+        printTree(node.right, indent + "    ");
+        System.out.println(indent + node.element);
+        printTree(node.left, indent + "    ");
+    }
+
+    private static class LeftistNode<E> {
+        LeftistNode(E element) {
+            this(element, null, null);
         }
 
-        n1.right = merge(n1.right, n2);
+        LeftistNode(E element, LeftistNode<E> left, LeftistNode<E> right) {
+            this.element = element;
+            this.left = left;
+            this.right = right;
+            this.pathToNull = 0;
+        }
 
-        if (n1 == null) {
-            n1.left = n1.right;
-            n1.right = null;
+        E element;
+        LeftistNode<E> left;
+        LeftistNode<E> right;
+        int pathToNull;
+    }
+
+    public static void main(String[] args) {
+        LeftistHeap<Integer> intHeap = new LeftistHeap<>();
+        for (int i = 0; i < 300; i += 10) {
+            intHeap.insert(i);
         }
-        else {
-            if (n1.left.pathToNull < n1.right.pathToNull) {
-                Node temp = n1.left;
-                n1.left = n1.right;
-                n1.right = temp;
-            }
-            n1.pathToNull = n1.right.pathToNull + 1;
+        for (int i = 0; i < 50; i++) {
+            intHeap.insert(i);
         }
-        return n1;
+        for (int i = 0; i < 25; i++) {
+            System.out.println(intHeap.deleteMin());
+        }
+        intHeap.printTree();
     }
 
 }
